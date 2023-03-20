1b674edb680c5375a1755236894a211f7a064dea
mr-dargi <dargimohammadreza@gmail.com>
2023-03-20 12:37:59 +0330

    Making routes for recive a json data from our login page and making connection to our database with mongoose package and making model for our database

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-const express = require("express");
+const express = require("express"); 
+const mongoose = require("mongoose");
+const User = require("./models/user");
 
-
+// middleware & json parser
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Listining on port 3000");
-})
+// mongodb connection with mongoose
+const dbURI = "mongodb://localhost:27017/users";
 
+mongoose.connect(dbURI)
+    .then(result => {
+        app.listen(3000, () => {
+            console.log("Listining on port 3000");
+        })
+    })
+    .catch(err => console.log(err));
+
+// listinig on port 3000
+
+
+// routes of the application
 app.get("/", (req, res) => {
     res.sendFile("./views/main.html", { root: __dirname });
+})
+
+
+app.post("/api/register", (req, res) => {
+    // console.log(req.body);
+    
+    res.json({ status: "ok" });
 })
